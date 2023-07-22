1ad96f5ef538c757abf68c4424b3c1bdfa199ff9
MoOsman5 <mohamed1592000gmail.com>
2023-07-23 00:35:10 +0200

    edit you may also like section

--- src/components/YouMayAlsoLike.jsx
+++ src/components/YouMayAlsoLike.jsx
@@ -14,7 +14,7 @@ const YouMayAlsoLike = () => {
     },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 3,
+      items: 4,
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
