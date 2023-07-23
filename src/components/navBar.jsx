4417a950f4ef80b349f184f0c125cc2afc4230ef
MoOsman5 <mohamed1592000gmail.com>
2023-07-23 03:19:50 +0200

    edit nav bar style

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -45,9 +45,9 @@ class NavBar extends Component {
             <li>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
+                width="22"
+                height="22"
+                viewBox="0 0 22 22"
                 fill="none"
               >
                 <svg
@@ -87,9 +87,9 @@ class NavBar extends Component {
             <li>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
+                width="22"
+                height="22"
+                viewBox="0 0 22 22"
                 fill="none"
               >
                 <g clip-path="url(#clip0_423_107)">
